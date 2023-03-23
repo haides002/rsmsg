8de24e92b5f0d6e1767f83e976d6950f39ab1029
Linus Pre <preiss.linus@gmail.com>
2023-03-23 22:18:05 +0100

    wowie

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
     println!("Hello, world!");
+    // what do we need?
+    // 1. skill
+    // 2. dummy chat (encrypted) chat should be a list of aes encrypted messages
+    // 3. decryption functions
+    // 4. encryption function
 }
