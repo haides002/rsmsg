2e86bf5b6f625f41e819ce6b89404e03a8dbe896
Keterion <HighTech-Nik@web.de>
2023-03-25 20:11:52 +0100

    Change from chacha20poly1305 to magic_crypt

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,24 @@
 mod cryption;
 pub use crate::cryption::*;
-use chacha20poly1305::{
-    aead::{Aead, AeadCore, KeyInit, OsRng},
-    ChaCha20Poly1305
-};
+
+use magic_crypt::{new_magic_crypt, MagicCryptTrait};
 
 fn main() {
     println!("Hello, world!");
-    cryption::crypting::decrypt();
-    cryption::crypting::encrypt();
-    cryption::crypting::decrypt();
-    cryption::crypting::encrypt();
+    // cryption::crypting::decrypt();
+    // cryption::crypting::encrypt();
 
     // https://docs.rs/chacha20poly1305/latest/chacha20poly1305/
     
-    let key = ChaCha20Poly1305::generate_key(&mut OsRng);
-    let cipher = ChaCha20Poly1305::new(&key);
-    let nonce = ChaCha20Poly1305::generate_nonce(&mut OsRng);
-    let ciphertext = cipher.encrypt(&nonce, b"Hello World!".as_ref()).unwrap();
-    let plaintext = cipher.decrypt(&nonce, ciphertext.as_ref()).unwrap();
-    assert_eq!(&plaintext, b"Hello World!");
-    println!("key: {:?}\ncipher: {:?}\nplain: {}",
-        key,
-        ciphertext,
-        String::from_utf8_lossy(&plaintext)
-    );
+    
+    let crypt_algorithm = new_magic_crypt!("UwU", 256);
+
+    let base64 = crypt_algorithm.encrypt_bytes_to_base64("Rawr");
+    println!("{}", base64);
+    assert_eq!("NQ1QnCJtwBLjcDBVdto8xQ==", base64);
+
+    assert_eq!("Rawr", crypt_algorithm.decrypt_base64_to_string(&base64).unwrap());
+
     // what do we need?
     // 1. skill
     // 2. dummy chat (encrypted) chat should be a list of aes encrypted messages
