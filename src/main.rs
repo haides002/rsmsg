784b67e0e7fd2bea0919de421d75aec990b0aa71
Linus Pre <preiss.linus@gmail.com>
2023-03-25 20:16:34 +0100

    fix bad merge

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,14 @@ use magic_crypt::{new_magic_crypt, MagicCryptTrait};
 
 fn main() {
     println!("Hello, world!");
-<<<<<<< HEAD
-    // cryption::crypting::decrypt();
-    // cryption::crypting::encrypt();
-=======
-    cryption::crypting::decrypt();
-    cryption::crypting::encrypt();
->>>>>>> 99f6b769b7051983d6f97bfcbbd4ee88e59f94b2
+    //cryption::crypting::decrypt();
+    //cryption::crypting::encrypt();
 
-    // https://docs.rs/chacha20poly1305/latest/chacha20poly1305/
-    
     
     let crypt_algorithm = new_magic_crypt!("UwU", 256);
-
     let base64 = crypt_algorithm.encrypt_bytes_to_base64("Rawr");
     println!("{}", base64);
     assert_eq!("NQ1QnCJtwBLjcDBVdto8xQ==", base64);
-
     assert_eq!("Rawr", crypt_algorithm.decrypt_base64_to_string(&base64).unwrap());
 
     // what do we need?
