29c4788d403af837b8ddb2aeebacc49d0b3dfeeb
Keterion <HighTech-Nik@web.de>
2023-03-25 19:08:56 +0100

    Add exemplar encryption with chacha20poly1305
    Idk how it does it or what a nonce is but it works

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
 mod cryption;
 pub use crate::cryption::*;
+use chacha20poly1305::{
+    aead::{Aead, AeadCore, KeyInit, OsRng},
+    ChaCha20Poly1305
+};
 
 fn main() {
     println!("Hello, world!");
     cryption::Crypting::decrypt();
     cryption::Crypting::encrypt();
+
+    // https://docs.rs/chacha20poly1305/latest/chacha20poly1305/
+    
+    let key = ChaCha20Poly1305::generate_key(&mut OsRng);
+    let cipher = ChaCha20Poly1305::new(&key);
+    let nonce = ChaCha20Poly1305::generate_nonce(&mut OsRng);
+    let ciphertext = cipher.encrypt(&nonce, b"Hello World!".as_ref()).unwrap();
+    let plaintext = cipher.decrypt(&nonce, ciphertext.as_ref()).unwrap();
+    assert_eq!(&plaintext, b"Hello World!");
+    println!("key: {:?}\ncipher: {:?}\nplain: {}",
+        key,
+        ciphertext,
+        String::from_utf8_lossy(&plaintext)
+    );
     // what do we need?
     // 1. skill
     // 2. dummy chat (encrypted) chat should be a list of aes encrypted messages
