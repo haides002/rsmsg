8b09a1340f0034ad2c10672342f034bfec0c20ca
zxmon-code <simonnomis.fr@gmail.com>
2023-04-02 14:25:01 +0200

    adds password prompt

--- src/ui.rs
+++ src/ui.rs
@@ -1,5 +1,19 @@
+use std::io;
+use std::io::Write;
+
 pub fn display_messages(messages: &Vec<String>) {
+    _ = clearscreen::clear();
     for message in messages {
         println!("{}\n", message);
     }
+}
+
+pub fn ask_password() -> String {
+    _ = clearscreen::clear();
+    let mut password = String::new();
+    print!("Enter your password: ");
+    _ = std::io::stdout().flush();
+    io::stdin().read_line(&mut password).unwrap();
+    _ = clearscreen::clear();
+    password
 }
